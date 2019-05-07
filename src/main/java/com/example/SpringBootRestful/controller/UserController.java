c785fe853a9cfc9de43053a980d7af24ecb107b1
admin <linwenjun@163.com>
2019-05-07 02:40:15 +0000

    first commit

---
+++ src/main/java/com/example/SpringBootRestful/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootRestful.controller;
+
+public class UserController {
+
+//todo: 在下面Controller 实现一个接口满足以下要求
+// 1. 实现一个Get请求,查出所有的用户信息
+// 2. 接口访问地址：（/api/users）
+// 3. 使用注解：@CrossOrigin实现跨域请求。
+
+}
